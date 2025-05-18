9ac5a2e2b594db68a8cca0f059ad21942bb80f13
yooni825 <104126233+askjiyun@users.noreply.github.com>
2025-05-19 08:44:46 +0900

    change descriptions

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,184 +1,142 @@
 $(document).ready(() => {
-    render_projects('featured');
-})
-
+  render_projects("featured");
+});
 
 let render_projects = (slug) => {
-    let projects_area = $('.projects-wrapper');
+  let projects_area = $(".projects-wrapper");
 
-    $('.white-button').removeClass('white-button-hover');
-    $(`#${slug}`).addClass('white-button-hover');
+  $(".white-button").removeClass("white-button-hover");
+  $(`#${slug}`).addClass("white-button-hover");
 
-    let projects_obj = [
-        {
-            image: 'assets/images/mentors.jpg',
-            link: 'https://github.com/abhn/Mporter',
-            title: 'Mporter',
-            demo: 'https://mporter.co',
-            technologies: ['Flask', 'Celery', 'Python'],
-            description: "Flask web application for easy reporting updates to one's mentor. Multi-user support, easy to deploy and use.",
-            categories: ['featured', 'webdev']
-        },
-        {
-            image: 'assets/images/mobile-landscape.jpg',
-            link: 'https://github.com/abhn/Wall-E',
-            title: 'Wall-E',
-            demo: 'http://wall-e-jekyll.github.io/',
-            technologies: ['Semantic UI', 'Jekyll'],
-            description: "A modern Jekyll theme with grid frontpage, beautiful typography, mobile responsive, made with Semantic UI.",
-            categories: ['featured', 'webdev']
-        },
-        {
-            image: 'assets/images/collage.jpg',
-            link: 'https://github.com/abhn/Marvel',
-            title: 'Marvel',
-            demo: false,
-            technologies: ['Android', 'OpenCV'],
-            description: "Attendance marking tool that uses face recognition for marking attendance and firebase for tracking and analytics.",
-            categories: ['featured', 'native']
-        },
-        {
-            image: 'assets/images/mpw.jpg',
-            link: 'https://github.com/abhn/mpw',
-            title: 'Master Password',
-            demo: 'https://www.nagekar.com/mpw',
-            technologies: ['Semantic UI', 'CSS3'],
-            description: "Master Password is an ingenious password solution that makes your passwords truly impossible to lose.",
-            categories: ['featured', 'security']
-        },
-        {
-            image: 'assets/images/social-share-count.jpeg',
-            link: 'https://github.com/abhn/Social-Share-Counts',
-            title: 'Social Share Count',
-            demo: false,
-            technologies: ['Python'],
-            description: "Ever wondered how many times a URL has been shared on popular social networks?",
-            categories: ['native']
-        },
-        {
-            image: 'assets/images/data-destroyer.png',
-            link: 'https://github.com/abhn/data-destroyer-gui',
-            title: 'Data Destroyer',
-            demo: false,
-            technologies: ['C++', 'Qt'],
-            description: "Native GUI wrapper for GNU coreutils tool 'dd'",
-            categories: ['native']
-        },
-        {
-            image: 'assets/images/raspberry-pi-monitor.png',
-            link: 'https://github.com/abhn/RPi-Status-Monitor',
-            title: 'Raspberry Pi Monitor',
-            demo: false,
-            technologies: ['python', 'flask'],
-            description: "Web based status monitor/smart mirror, displays system stats, weather and more.",
-            categories: ['webdev', 'diy']
-        },
-        {
-            image: 'assets/images/s3scan.png',
-            link: 'https://github.com/abhn/S3Scan',
-            title: 'S3Scan',
-            demo: false,
-            technologies: ['python'],
-            description: "Automate crawling of a website and find publicly open S3 buckets for takeover.",
-            categories: ['native', 'security']
-        },
-        {
-            image: 'assets/images/elementary.png',
-            link: 'https://github.com/abhn/Elementary',
-            title: 'Elementary',
-            demo: 'https://elementary-jekyll.github.io/',
-            technologies: ['Jekyll', 'CSS3'],
-            description: "Elementary is a zero Javascript and minimal CSS ultra lightweight Jekyll theme for those of you who love simplicity.",
-            categories: ['webdev']
-        },
-        {
-            image: 'assets/images/soot-spirits.png',
-            link: 'https://github.com/abhn/Soot-Spirits',
-            title: 'Soot Spirits',
-            demo: 'https://sootspirits.github.io',
-            technologies: ['Jekyll', 'CSS3'],
-            description: "A simple responsive two column Jekyll theme. Great for personal blog and basic portfolio website.",
-            categories: ['webdev']
-        },
-        {
-            image: 'assets/images/python-chat.png',
-            link: 'https://www.nagekar.com/2014/12/lan-group-messenger-in-python.html',
-            title: 'Terminal Group Chat',
-            demo: false,
-            technologies: ['Python', 'Sockets'],
-            description: "Simple terminal group chat based on native sockets using Python.",
-            categories: ['native']
-        },
-        {
-            image: 'assets/images/old-lcd.jpg',
-            link: 'https://www.nagekar.com/2018/05/reusing-old-laptop-lcd-panel.html',
-            title: 'Reusing Old LCD Panel',
-            demo: false,
-            technologies: ['DIY'],
-            description: "Reusing a dead laptop's LCD panel as a secondary monitor.",
-            categories: ['diy']
-        },
-        {
-            image: 'assets/images/nextcloud-enc.png',
-            link: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Encrypted Self-Hosted Cloud',
-            demo: false,
-            technologies: ['NextCloud', 'GnuPG'],
-            description: "Self hosted encrypted cloud setup with Nextcloud and GnuPG.",
-            categories: ['diy', 'security']
-        },
-        {
-            image: 'assets/images/google-cloud-backup.png',
-            link: 'https://www.nagekar.com/2018/05/encrypted-backup-with-duplicity.html',
-            title: 'Encrypted Backups - Google Cloud',
-            demo: false,
-            technologies: ['NextCloud', 'Duplicity'],
-            description: "Create automated encrypted incremental backups of data. Sync everything securely to Google Cloud.",
-            categories: ['diy', 'security']
-        },
-        {
-            image: 'assets/images/pi-cloud.jpg',
-            link: 'https://www.nagekar.com/2016/01/how-to-private-local-cloud-using-raspberrypi.html',
-            title: 'Local Cloud - Raspberry Pi',
-            demo: false,
-            technologies: ['FTP', 'DIY'],
-            description: "Host a local cloud server with a Raspberry Pi and a spare hard disk. Access data instantaneously on any device on the network.",
-            categories: ['diy']
-        },
-        {
-            image: 'assets/images/koalamate.png',
-            link: 'https://github.com/abhn/koalamate',
-            title: 'Koalamate',
-            demo: false,
-            technologies: ['Electron', 'Javascript'],
-            description: "A cross-platform desktop application that serves as a Wolfram Alpha query place and notes taker.",
-            categories: ['native']
-        },
-    ]
+  let projects_obj = [
+    {
+      image: "assets/images/camchelin.png",
+      link: "https://github.com/CineNorte-WEB/DeepLearning",
+      title: "Camchelin",
+      demo: false,
+      technologies: ["NLP", "Python"],
+      description:
+        "Developed a sentiment analysis and keyword extraction model for restaurants using user and platform reviews, followed by category-wise sentence generation.",
+      categories: ["all", "tave"],
+    },
+    {
+      image: "assets/images/tobemom.png",
+      link: "https://github.com/GDSC-23-24-BABY-APP/front-end",
+      title: "To Be Mom",
+      demo: false,
+      technologies: ["Android Studio", "Kotlin"],
+      description:
+        "Google Solution Challenge 2024 - A healthcare application designed to help pregnant women monitor their condition and access essential information to reduce miscarriage and stillbirth risks.",
+      categories: ["all", "gdsc"],
+    },
+    {
+      image: "assets/images/momentum.png",
+      link: "https://github.com/codeit-momentum/moment-front",
+      title: "Momentum",
+      demo: "https://www.codeit-momentum.shop/",
+      technologies: ["React", "Typescript"],
+      description:
+        "Award-winning project at Codeit PowerBoost Demo Day — a goal-tracking app that helps users break down goals into manageable moments with checklist-based progress tracking.",
+      categories: ["all", "codeit"],
+    },
+    {
+      image: "assets/images/MediMate.png",
+      link: "https://github.com/software-competition-2024/Front-end",
+      title: "Medimate",
+      demo: false,
+      technologies: ["React Native", "Javascript"],
+      description:
+        "Award-winning project in the senior division of an IT competition — a mobile app for managing medication expiration dates using OCR technology",
+      categories: ["all", "contest"],
+    },
+    {
+      image: "assets/images/masterkey.png",
+      link: "https://github.com/Kim-Yun-Seo/MasterKey",
+      title: "MasterKey",
+      demo: false,
+      technologies: ["Kotlin", "Android"],
+      description:
+        "A kiosk practice platfrom designed to support digital literacy for vulnerable groups, developed as a final project for the Mobile Programming course.",
+      categories: ["all", "lecture"],
+    },
+    {
+      image: "assets/images/white.jpg",
+      link: "https://github.com/askjiyun/DataStructure_Project",
+      title: "Data Structure Project",
+      demo: false,
+      technologies: ["Python", "Data Structures", "Algorithms"],
+      description:
+        "Planned and implemented a shortest path algorithm to help socially disadvantaged individuals navigate museums easily, as part of a Data Structures course.'",
+      categories: ["all", "lecture"],
+    },
+    {
+      image: "assets/images/white.jpg",
+      link: "https://github.com/askjiyun/diffusion_model",
+      title: "Diffusion Model Study",
+      demo: false,
+      technologies: ["Python", "Diffusion"],
+      description:
+        "Undergraduate research project focused on studying the theory and implementation of Diffusion Models, including hands-on experiments and paper reviews.",
+      categories: ["all", "lab"],
+    },
+    {
+      image: "assets/images/white.jpg",
+      link: "https://github.com/Sungshin-Capstone/ezpage_AI",
+      title: "Capstone Design Project",
+      demo: false,
+      technologies: ["Python", "CV", "ML"],
+      description:
+        "Capstone project that developed a travel assistant app using OCR and image classification models to recognize and manage foreign currency expenses.",
+      categories: ["all", "lecture"],
+    },
+    {
+      image: "assets/images/white.jpg",
+      link: "https://github.com/askjiyun/recommender-system/tree/main",
+      title: "Recommender System",
+      demo: false,
+      technologies: ["Recommender System"],
+      description:
+        "Practice and assignments for the Recommender Systems course in the first semester of 2024",
+      categories: ["all", "lecture"],
+    },
+    {
+      image: "assets/images/white.jpg",
+      link: "https://github.com/askjiyun/BigDataAnalysis/tree/main",
+      title: "Big Data Analysis",
+      demo: false,
+      technologies: ["Big Data", "Python"],
+      description:
+        "Practice and assignments for the Big Data Analysis course in the second semester of 2024",
+      categories: ["all", "lecture"],
+    },
+  ];
 
-    let projects = [];
-    if(slug == 'all') {
-        projects = projects_obj.map(project_mapper);
-    } 
-    else {
-        projects = projects_obj.filter(project => project.categories.includes(slug)).map(project_mapper);
-    }
-    projects_area.hide().html(projects).fadeIn();
-}
+  let projects = [];
+  if (slug == "all") {
+    projects = projects_obj.map(project_mapper);
+  } else {
+    projects = projects_obj
+      .filter((project) => project.categories.includes(slug))
+      .map(project_mapper);
+  }
+  projects_area.hide().html(projects).fadeIn();
+};
 
-let project_mapper = project => {
-    return `
+let project_mapper = (project) => {
+  return `
         <div class="wrapper">
                 
             <div class="card radius shadowDepth1">
 
-                ${project.image ? 
-                    `<div class="card__image border-tlr-radius">
+                ${
+                  project.image
+                    ? `<div class="card__image border-tlr-radius">
                         <a href="${project.link}">
                             <img src="${project.image}" alt="image" id="project-image" class="border-tlr-radius">
                         </a>
-                    </div>`           
-                : ''}
+                    </div>`
+                    : ""
+                }
 
         
                 <div class="card__content card__padding">
@@ -186,22 +144,29 @@ let project_mapper = project => {
                     <article class="card__article">
                         <h2><a href="${project.link}">${project.title}</a></h2>
         
-                        <p class="paragraph-text-normal">${project.description} ${project.demo ? `<a href="${project.demo}">Demo</a>` : ''}</p>
+                        <p class="paragraph-text-normal">${
+                          project.description
+                        } ${
+    project.demo ? `<a href="${project.demo}">Demo</a>` : ""
+  }</p>
                     </article>
 
                                 
                     <div class="card__meta">
-                        ${project.technologies.map(tech =>
-                            `<span class="project-technology paragraph-text-normal">${tech}</span>`
-                        ).join('')}
+                        ${project.technologies
+                          .map(
+                            (tech) =>
+                              `<span class="project-technology paragraph-text-normal">${tech}</span>`
+                          )
+                          .join("")}
                     </div>
 
                 </div>
             </div>
         </div>
-    `
-}
+    `;
+};
 
 let selected = (slug) => {
-    render_projects(slug);
-}
+  render_projects(slug);
+};
